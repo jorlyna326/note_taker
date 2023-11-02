e969649f356f8e8d1f57b2993b7e583e3f1003e8
Jorlyna Pujols <jorlynapujols@Jorlynas-MBP.attlocal.net>
2023-11-01 23:02:50 -0400

    added starter code and built express server

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs')
+
+// Import the feedback router
+const notes = require('./db/db.json')
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Middleware to serve up static assets from the public folder
+app.use(express.static('public'));
+
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
